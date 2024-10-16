626e2f7cf39cfca7682624bec296401a33e95f6c
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-10-16 10:06:35 +0500

    handle add blocks

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import GradientButton from './Components/Gradient';
 
 const App = () => {
-  const [gradient, setGradient] = useState<string>('linear-gradient(to right, rgb(0, 0, 0), rgb(255, 255, 255))');
+  const [blocks, setBlocks] = useState<Array<string>>([]);
+  const handleAddBlock =()=> {
+    setBlocks([...blocks , 'linear-gradient(to right, rgb(0,0,0), rgb(255,255,255))']);
+  }
 
-  const handleGradientChange = (newGradient: string) => {
-    setGradient(newGradient);
-  };
 
   return (
     <div className="app" style={{ background: gradient }}>
